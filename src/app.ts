f07f48de24a890e22ebdb4a999d17ba839244fa5
Matt Westwick <matt.westwick@gmail.com>
2020-12-23 23:17:56 -0600

    initial project setup

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to Hipster Castle!</h1>');
+});
+
+app.listen(port, () => {
+  return console.log(`server is listening on ${port}`);
+});
